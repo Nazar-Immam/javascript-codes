92b85014f34f7db4ae05c11ba75347c83d969898
Nazar-Immam <156925750+Nazar-Immam@users.noreply.github.com>
2025-01-19 23:44:29 +0000

    datatypes&variables

---
+++ basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" ; // this treates all js codes as newer version of js
+
+//data types
+
+let name = "Immam" //=>string
+let age = 21 // =>integer of range 2^53 , for larger int value use bigint ;
+let isLoggedIN = false //=> boolean ;
+let email ; //=> undefined ;
+
+/* more data types
+    null => this means empty
+    undefined => this happens when data type and variable is declared but not initialised.
+    symbol => used for uniqueness 
+    object => later..
+*/
+
+// to know the type of any data use
+// typeof
+
+console.log(typeof "Immam");
+console.log(typeof 20) ;
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId= 12343  //const cannot be changed further 
+let accountEmail ="immam@mail.com"
+var accountPassword= "skdfj"
+//do not prefer var
+city ="Jaipur"
+console.log(city);
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountId]);
+
+
